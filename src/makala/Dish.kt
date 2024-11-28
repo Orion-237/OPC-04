9135c97ddd7e29789bc0ce5838a6080da6bd87d2
Alex Nelson Ryan <142633593+NinjaShadowBoy@users.noreply.github.com>
2024-11-28 17:27:31 +0100

    Created a Main file and a greetings function in the main file

--- src/makala/Dish.kt
+++ src/makala/Dish.kt
@@ -1,4 +1,6 @@
 package makala
 
-class Dish {
+class Dish(var name: String, var price: Double, var qty: Int = 1) {
+    fun details(): String = "$name - Price: $price XAF Qty: $qty Total: ${total()}"
+    fun total() = price * qty
 }
--- src/makala/Order.kt
+++ src/makala/Order.kt
@@ -1,4 +1,24 @@
 package makala
 
-class Order {
+class Order(var name: String = "No Name") {
+    companion object {
+        var i = 0
+        fun nextOrderNumber() = ++i
+        var discountPercentage = 0.05
+    }
+    var id = "CMD${nextOrderNumber()}"
+    val dishes: MutableList<Dish> = mutableListOf()
+
+    fun details(): String {
+        var result: String = "📝Details of order $id by $name:\n"
+        dishes.forEachIndexed { i, dish ->
+            result += "\t${i+1}. ${dish.details()}\n"
+        }
+        return result
+    }
+    fun addDish(dish: Dish) {
+        dishes.add(dish)
+    }
+    fun totalPrice(): Double = dishes.sumOf { it.total() }
+    fun discountedPrice(): Double = discountPercentage*discountedPrice()
 }
